250f81050bb1896a6e67a02db5f58af0250456ff
August Hammers <augusthammers@Xubuntu.myguest.virtualbox.org>
2023-12-05 19:10:52 -0500

    Add form, validation

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('hi world');
+function checkPassword() {
+    const pw = document.getElementById('password')
+    const pwConfirm = document.getElementById('pwconfirm')
+
+    if (pw.value == pwConfirm.value) {
+        pw.classList.remove("error")
+        pwConfirm.classList.remove("error")
+    } else {
+        pw.classList.add("error")
+        pwConfirm.classList.add("error")
+    }
+}
